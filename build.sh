550a6a00554413e20d5d021204537d4e2ec92a62
Jonny Bergdahl <bergdahl@users.noreply.github.com>
2023-11-03 17:30:37 +0100

    Finalized code

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Build docs
+doxygen Doxyfile
+open extras/html/index.html
--- examples/LoggingExample/LoggingExample.ino
+++ examples/LoggingExample/LoggingExample.ino
@@ -1,39 +1,83 @@
-#include <SimpleLogger.h>
+#include <jblogger.h>
 
-JBLogger log("Example", LOG_LEVEL_DEBUG, Serial
-uint32_t index = 0;
-char* text = "Included text";
+// Create a JBLogger instance with the module name "LOG" and set the log level to LOG_LEVEL_TRACE.
+JBLogger logger("LOG", LogLevel::LOG_LEVEL_TRACE);
+//
+// You can also use a different Print class for output, for example Serial1:
+//
+//JBLogger logger("LOG", LogLevel::LOG_LEVEL_TRACE, Serial1);
 
-void setup() {
-	Serial.begin(115200);
-}
+uint32_t counter = 0;                    // Integer value for logging
+const char* text = "Included text";             // Text for logging
+const char* buffer = "Text\r\nMore text\t\x7f 12345\xff"; // Binary data buffer for logging
 
+// Here we execute logging with different log levels and different types of
+// messages.
 void logMessages() {
-	log.error(LOG_LEVEL_ERROR, "This is an error message.");
-	log.warning("This is a formatted warning message, index: %d", index);
-	log.info("This is an info message with %d", 42);
-	log.debug("This is a debug message with %s", included text);
-	index++;
+	logger.error("This is an error message.");
+	logger.warning("This is a formatted warning message: %d", counter);
+	logger.info("This is a formatted info message: %d", 42);
+	logger.debug("This is a formatted debug message: %s", text);
+	logger.trace("This is a formatted TRACE message: %d", counter);
+	logger.trace("traceDump:");
+	logger.traceDump(buffer, strlen(buffer));
+	logger.trace("traceHexDump:");
+	logger.traceHexDump(buffer, strlen(buffer));
+	logger.trace("traceAsciiDump");
+	logger.traceAsciiDump(buffer, strlen(buffer));
+	counter++;
 }
 
+// Initialize Serial and wait for it to be ready.
+void setup() {
+	// Since we are using Serial for output we need to start it.
+	Serial.begin(115200);
+	while (!Serial) {};  // Wait for Serial to be ready
+}
+
+// Here we run the logMessages() function with log levels and log settings.
 void loop() {
-	Serial.println("Logging example, Log level: DEBUG");
+	logger.setLogLevel(LOG_LEVEL_ERROR);
+	logger.error("Logging example, Log level: ERROR");
 	logMessages();
-	delay(100);
-	log.setLogLevel(LOG_LEVEL_INFO);
-	Serial.println("Logging example, Log level: INFO");
+
+	logger.setLogLevel(LOG_LEVEL_WARNING);
+	logger.warning("Logging example, Log level: WARNING");
 	logMessages();
-	delay(100);
-	log.setLogLevel(LOG_LEVEL_WARNING);
-	Serial.println("Logging example, Log level: WARNING");
+
+	logger.setLogLevel(LOG_LEVEL_INFO);
+	logger.info("Logging example, Log level: INFO");
 	logMessages();
-	delay(100);
-	log.setLogLevel(LOG_LEVEL_ERROR);
-	Serial.println("Logging example, Log level: ERROR");
+
+	logger.setLogLevel(LOG_LEVEL_DEBUG);
+	logger.debug("Logging example, Log level: DEBUG");
 	logMessages();
-	delay(100);
-	log.setLogLevel(LOG_LEVEL_NONE);
-	Serial.println("Logging example, Log level: NONE");
+
+	logger.setLogLevel(LOG_LEVEL_TRACE);
+	logger.trace("Logging example, Log level: TRACE");
 	logMessages();
-	delay(30000);
+
+	logger.setLogLevel(LOG_LEVEL_NONE);
+	logger.trace("Logging example, Log level: None");
+	logMessages();
+
+	logger.setLogLevel(LOG_LEVEL_INFO);
+	logger.setShowLogLevel(false);
+	logger.info("Disabled Show log level");
+	logMessages();
+
+	logger.setShowModuleName(false);
+	logger.info("Disabled Show module name");
+	logMessages();
+
+	logger.setShowTimestamp(false);
+	logger.info("Disabled Show timestamps");
+	logMessages();
+
+	logger.info("Sleeping for 10 seconds");
+	delay(10000);
+
+	logger.setShowLogLevel(true);
+	logger.setShowModuleName(true);
+	logger.setShowTimestamp(true);
 }
